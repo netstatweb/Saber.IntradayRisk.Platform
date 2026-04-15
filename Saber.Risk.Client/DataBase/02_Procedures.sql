808ece46bfe8a7a33d2619b2643b08cfca292475
Maciej Kurowski <netstatweb@gmail.com>
2026-04-16 01:21:36 +0200

    feat: add SQL Market Engine with 100k positions simulation

---
+++ Saber.Risk.Client/DataBase/02_Procedures.sql
@@ -0,0 +1,38 @@
+﻿GO
+CREATE PROCEDURE sp_MarketPulse
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @Vol FLOAT = (SELECT SettingValue FROM MarketSettings WHERE SettingKey = 'Volatility');
+    
+    -- Symulujemy ruchy tylko na części rynku (realizm płynności)
+    UPDATE TOP (5) PERCENT CurrentRisk
+    SET Delta = Delta + ((RAND() - 0.5) * 0.05 * @Vol),
+        Exposure = Exposure + ((RAND() - 0.5) * 1000 * @Vol),
+        LastUpdate = SYSDATETIME();
+
+    -- Zwracamy tylko to, co się zmieniło w ciągu ostatniej sekundy
+    SELECT * FROM CurrentRisk WHERE LastUpdate >= DATEADD(SECOND, -1, SYSDATETIME());
+END
+
+GO
+CREATE PROCEDURE sp_GenerateInitialData
+AS
+BEGIN
+    SET NOCOUNT ON;
+    TRUNCATE TABLE CurrentRisk;
+    DECLARE @i INT = 1;
+    WHILE @i <= 100000
+    BEGIN
+        INSERT INTO CurrentRisk (Ticker, Book, Currency, Delta, Gamma, Vega, Exposure)
+        VALUES (
+            'T-' + RIGHT('00000' + CAST(@i AS VARCHAR), 6),
+            CASE WHEN @i%4=0 THEN 'FX_EMEA' WHEN @i%4=1 THEN 'FX_APAC' WHEN @i%4=2 THEN 'COMM_US' ELSE 'EQUITY' END,
+            CASE WHEN @i%3=0 THEN 'USD' WHEN @i%3=1 THEN 'EUR' ELSE 'GBP' END,
+            RAND()-0.5, RAND()*0.1, RAND()*0.5, RAND()*100000
+        );
+        SET @i = @i + 1;
+    END
+END
+GO
+EXEC sp_GenerateInitialData;
---
+++ Saber.Risk.Client/Models/RiskMetric.cs
@@ -0,0 +1,30 @@
+﻿using Saber.Risk.Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Saber.Risk.Client.Models
+{
+    public class RiskMetric : ViewModelBase
+    {
+        public string Ticker { get; set; }
+        public string Book { get; set; }
+
+        private double _delta;
+        public double Delta
+        {
+            get => _delta;
+            set { _delta = value; OnPropertyChanged(); }
+        }
+
+        private double _gamma;
+        public double Gamma
+        {
+            get => _gamma;
+            set { _gamma = value; OnPropertyChanged(); }
+        }
+    }
+}
